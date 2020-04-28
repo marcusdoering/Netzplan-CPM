41fb51349661ea4e2c490e41cfb98d424b9882d3
marcusdoering <49989788+marcusdoering@users.noreply.github.com>
2020-04-28 14:48:28 +0200

    adjusted testing examples in __main__ for tut img

--- src/Process.py
+++ src/Process.py
@@ -1,5 +1,12 @@
 class Process:
-    def __init__(self, id, name, dauer):
+    def __init__(self, id: int, name: str, duration: int):
+        """
+        Constructor-Funktion, welche beim Erstellen eines Objekts aufgerufen wird.
+
+        :param id: Die ID eines Prozesses
+        :param name: Name eines Prozesses
+        :param duration: Dauer bzw Laenge eines Prozesses
+        """
         self.faz = None
         self.fez = None
         self.saz = None
@@ -8,11 +15,17 @@ class Process:
         self.name = name
         self.gp = None
         self.fp = None
-        self.dauer = dauer
+        self.duration = duration
+        # Leere List-Variablen, vergleichbar mit ArrayLists in Java
         self.predecessor = []
         self.successor = []
 
     def get_id(self):
+        """
+        Getter-Methode
+
+        :return: Die ID eines Prozesses
+        """
         return self.id
 
     def set_id(self, new_id: int):
@@ -25,18 +38,30 @@ class Process:
         self.name = new_name
 
     def get_dauer(self):
-        return self.dauer
+        return self.duration
 
-    def set_dauer(self, new_dauer: int):
-        self.dauer = new_dauer
+    def set_dauer(self, new_duration: int):
+        self.duration = new_duration
 
     def calc_fez(self):
+        """
+        Berechnet den FEZ des Prozesses oder gibt einen angepassten Wert zurueck,
+        falls es sich um den Startprozess handelt
+
+        :return: none
+        """
         if self.is_start_process():
-            self.fez = self.dauer
+            self.fez = self.duration
         else:
-            self.fez = self.faz + self.dauer
+            self.fez = self.faz + self.duration
 
     def calc_faz(self):
+        """
+        Berechnet den FAZ des Prozesses oder gibt 0 zurueck, falls es sich um
+        den Startprozess handelt
+
+        :return: none
+        """
         if self.is_start_process():
             self.faz = 0
         else:
@@ -47,7 +72,7 @@ class Process:
             self.faz = current_max
 
     def calc_saz(self):
-        self.saz = self.sez - self.dauer
+        self.saz = self.sez - self.duration
 
     def calc_sez(self):
         if self.is_end_process():
@@ -60,7 +85,12 @@ class Process:
             self.sez = current_min
 
     def is_critical(self):
-        if self.faz == self.saz:
+        """
+        Gibt an, ob ein Prozess zum "kritischen Pfad" gehoert
+
+        :return: bool; je nachdem ob Prozess zum "kritischen Pfad" gehoert
+        """
+        if self.gp == 0:
             return True
         else:
             return False
--- src/__main__.py
+++ src/__main__.py
@@ -1,40 +1,50 @@
-from src.Process import Process
+from src.ProcessXT import ProcessXT
 
-proc1 = Process(1, "Hallo", 2)
-proc2 = Process(2, "Welt", 4)
-proc3 = Process(3, "Drei", 3)
+proc1 = ProcessXT(1, "A", 2)
+proc2 = ProcessXT(2, "B", 4)
+proc3 = ProcessXT(3, "C", 3)
+proc4 = ProcessXT(4, "D", 2)
+proc5 = ProcessXT(5, "E", 1)
+proc6 = ProcessXT(6, "F", 4)
+proc7 = ProcessXT(7, "G", 5)
 
 proc1.add_successor(proc2)
-proc2.add_predecessor(proc1)
 proc2.add_successor(proc3)
-proc3.add_predecessor(proc2)
+proc2.add_successor(proc4)
+proc3.add_successor(proc5)
+proc3.add_successor(proc6)
+proc4.add_successor(proc5)
+proc5.add_successor(proc7)
+proc6.add_successor(proc7)
 
-# id 1 dauer 5 vorher -
-# id 2 dauer 3 vorher 1
-# id 3 dauer 4 vorher 2
+proc2.add_predecessor(proc1)
+proc3.add_predecessor(proc2)
+proc4.add_predecessor(proc2)
+proc5.add_predecessor(proc3)
+proc5.add_predecessor(proc4)
+proc6.add_predecessor(proc3)
+proc7.add_predecessor(proc5)
+proc7.add_predecessor(proc6)
+
+list_of_proc = [proc1, proc2, proc3, proc4, proc5, proc6, proc7]
+
 
 # faz    fez
 # saz    sez
 
 # vorwaertsterminierung
-for element in [proc1, proc2, proc3]:
+for element in list_of_proc:
     element.calc_faz()
     element.calc_fez()
 
-for element in [proc1, proc2, proc3][::-1]:
+# rueckwaertsterminierung
+for element in list_of_proc[::-1]:
     element.calc_sez()
     element.calc_saz()
 
 # pufferberechnung
-for element in [proc1, proc2, proc3]:
-    element.gp = element.saz - element.faz
-    if len(element.successor) == 0:
-        element.fp = 0
-    else:
-        current_max = 0
-        for succ_element in element.successor:
-            if succ_element.faz > current_max:
-                current_max = succ_element.faz
-        element.fp = current_max - element.fez
+for element in list_of_proc:
+    element.calc_gp()
+    element.calc_fp()
 
 print()
