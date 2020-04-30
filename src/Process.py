b4ccc93ca464787a5eeb652ec2bf146057e0ac88
marcusdoering <49989788+marcusdoering@users.noreply.github.com>
2020-04-30 13:05:16 +0200

    added functions for pre/successor, added docs

--- src/Process.py
+++ src/Process.py
@@ -13,16 +13,16 @@ class Process:
         self.sez = None
         self.id = id
         self.name = name
+        self.duration = duration
         self.gp = None
         self.fp = None
-        self.duration = duration
         # Leere List-Variablen, vergleichbar mit ArrayLists in Java
         self.predecessor = []
         self.successor = []
 
     def get_id(self):
         """
-        Getter-Methode
+        Getter-Methode.
 
         :return: Die ID eines Prozesses
         """
@@ -32,12 +32,22 @@ class Process:
         self.id = new_id
 
     def get_name(self):
+        """
+        Getter-Methode.
+
+        :return: Den Namen eines Prozesses
+        """
         return self.name
 
     def set_name(self, new_name: str):
         self.name = new_name
 
     def get_dauer(self):
+        """
+        Getter-Methode.
+
+        :return: Die Dauer eines Prozesses
+        """
         return self.duration
 
     def set_dauer(self, new_duration: int):
@@ -46,19 +56,21 @@ class Process:
     def calc_fez(self):
         """
         Berechnet den FEZ des Prozesses oder gibt einen angepassten Wert zurueck,
-        falls es sich um den Startprozess handelt
+        falls es sich um den Startprozess handelt.
 
         :return: none
         """
         if self.is_start_process():
             self.fez = self.duration
-        else:
+        elif self.faz is not None:
             self.fez = self.faz + self.duration
+        else:
+            raise RuntimeError("FAZ has not been calculated for this object yet.")
 
     def calc_faz(self):
         """
         Berechnet den FAZ des Prozesses oder gibt 0 zurueck, falls es sich um
-        den Startprozess handelt
+        den Startprozess handelt.
 
         :return: none
         """
@@ -72,11 +84,28 @@ class Process:
             self.faz = current_max
 
     def calc_saz(self):
-        self.saz = self.sez - self.duration
+        """
+        Berechnet den SAZ des Prozesses. Der SEZ muss dazu gegeben sein.
+
+        :return: none
+        """
+        if self.sez is not None:
+            self.saz = self.sez - self.duration
+        else:
+            raise RuntimeError("SEZ has not been calculated for this object yet.")
 
     def calc_sez(self):
+        """
+        Berechnet den SEZ des Prozesses or gibt den FEZ zurueck, wenn es sich um
+        den Endprozess handelt.
+
+        :return: none
+        """
         if self.is_end_process():
-            self.sez = self.fez
+            if self.fez is not None:
+                self.sez = self.fez
+            else:
+                raise RuntimeError("FEZ has not been calculated for this object yet.")
         else:
             current_min = 9999
             for succ_element in self.successor:
@@ -86,7 +115,7 @@ class Process:
 
     def is_critical(self):
         """
-        Gibt an, ob ein Prozess zum "kritischen Pfad" gehoert
+        Gibt an, ob ein Prozess zum "kritischen Pfad" gehoert.
 
         :return: bool; je nachdem ob Prozess zum "kritischen Pfad" gehoert
         """
@@ -96,10 +125,36 @@ class Process:
             return False
 
     def add_predecessor(self, process):
+        """
+        Fuegt einen Prozess der Liste der vorhergehenden Prozesse hinzu.
+
+        :param process: Prozess, welcher der Liste hinzugefuegt wird
+        :return: none
+        """
         self.predecessor.append(process)
 
     def add_successor(self, process):
+        """
+        Fuegt eine Prozess der Liste der nachfolgenden Prozesse hinzu.
+
+        :param process: Prozess, welcher der Liste hinzugefuegt wird
+        :return: none
+        """
         self.successor.append(process)
+
+    def add_successor_and_predecessor(self, process):
+        """
+        Fuegt einen Prozess A der Liste der nachfolgenden Prozesse von Prozess B hinzu.
+        Prozess B wird danach der Liste der vorhergehenden Prozesse von Prozess A hinzugefuegt.
+
+        Wurde der Prozess bereits hinzugefuegt, so wird keine Aenderung vorgenommen.
+
+        :param process: Prozess, welcher als Nachfolger eingetragen wird
+        :return: none
+        """
+        if process not in self.successor:
+            self.successor.append(process)
+            process.add_predecessor(self)
 
     def is_predecessor(self, process):
         if process.id in self.predecessor:
--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,6 @@
 from src.ProcessXT import ProcessXT
 
+# Prozesse werden erstellt
 proc1 = ProcessXT(1, "A", 2)
 proc2 = ProcessXT(2, "B", 4)
 proc3 = ProcessXT(3, "C", 3)
@@ -8,41 +9,33 @@ proc5 = ProcessXT(5, "E", 1)
 proc6 = ProcessXT(6, "F", 4)
 proc7 = ProcessXT(7, "G", 5)
 
-proc1.add_successor(proc2)
-proc2.add_successor(proc3)
-proc2.add_successor(proc4)
-proc3.add_successor(proc5)
-proc3.add_successor(proc6)
-proc4.add_successor(proc5)
-proc5.add_successor(proc7)
-proc6.add_successor(proc7)
+# Nachfolger und Vorgaenger werden angegeben
+proc1.add_successor_and_predecessor(proc2)
+proc2.add_successor_and_predecessor(proc3)
+proc2.add_successor_and_predecessor(proc4)
+proc3.add_successor_and_predecessor(proc5)
+proc3.add_successor_and_predecessor(proc6)
+proc4.add_successor_and_predecessor(proc5)
+proc5.add_successor_and_predecessor(proc7)
+proc6.add_successor_and_predecessor(proc7)
 
-proc2.add_predecessor(proc1)
-proc3.add_predecessor(proc2)
-proc4.add_predecessor(proc2)
-proc5.add_predecessor(proc3)
-proc5.add_predecessor(proc4)
-proc6.add_predecessor(proc3)
-proc7.add_predecessor(proc5)
-proc7.add_predecessor(proc6)
-
+# Liste aller Prozesse wird erstellt
 list_of_proc = [proc1, proc2, proc3, proc4, proc5, proc6, proc7]
-
 
 # faz    fez
 # saz    sez
 
-# vorwaertsterminierung
+# Vorwaertsterminierung
 for element in list_of_proc:
     element.calc_faz()
     element.calc_fez()
 
-# rueckwaertsterminierung
+# Rueckwaertsterminierung
 for element in list_of_proc[::-1]:
     element.calc_sez()
     element.calc_saz()
 
-# pufferberechnung
+# Pufferberechnung
 for element in list_of_proc:
     element.calc_gp()
     element.calc_fp()
